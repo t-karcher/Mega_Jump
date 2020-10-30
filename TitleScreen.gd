7562a7ac32241caacac7b5eb0a29da1851ae3f56
Thomas Karcher <t_karcher@posteo.de>
2020-10-30 11:17:14 +0100

    Music added to title screen

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -2,5 +2,14 @@ extends Node2D
 
 var game_scene: PackedScene = load("res://World.tscn")
 
+func _ready():
+	$AudioStreamPlayer.play()
+
 func _on_PlayButton_pressed():
 	get_tree().change_scene_to(game_scene)
+
+func _on_ToggleMusicButton_toggled(button_pressed):
+	if button_pressed:
+		$AudioStreamPlayer.play()
+	else:
+		$AudioStreamPlayer.stop()
